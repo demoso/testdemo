02afcc37977bf4925474f974a89c462640b9dcf6
Administrator <admin@example.com>
2020-09-06 21:10:38 +0800

    7

--- src/main/java/com/example/demo/entity/TestRole.java
+++ src/main/java/com/example/demo/entity/TestRole.java
@@ -1,8 +1,10 @@
 package com.example.demo.entity;
 
 import lombok.Data;
+import lombok.experimental.Accessors;
 
 @Data
+@Accessors(chain = true)
 public class TestRole {
     private  Long id;
     private  String roleName;
